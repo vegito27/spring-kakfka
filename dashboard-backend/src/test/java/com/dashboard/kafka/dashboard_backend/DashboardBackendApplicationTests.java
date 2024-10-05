31fd71be0bbf0632d082c25b4204877ef17f9962
Rishabh Tripathi <rishabh.tripathi@crownstack.com>
2024-10-05 11:16:26 +0530

    started project on kafka

---
+++ dashboard-backend/src/test/java/com/dashboard/kafka/dashboard_backend/DashboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.kafka.dashboard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
